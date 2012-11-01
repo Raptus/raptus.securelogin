1e54e0ceaaa67adccbf2ce82b51cabe0a60210d2
Jonas Wagner <jwagner@raptus.com>
2012-11-01 16:03:36 +0100

    Moved to github

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ version = '1.0a2'
 setup(name='raptus.securelogin',
       version=version,
       description="Provides a more secure login procedure for Plone for a defined set of groups.",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
@@ -16,7 +16,7 @@ setup(name='raptus.securelogin',
       keywords='',
       author='Raptus AG',
       author_email='dev@raptus.com',
-      url='https://svn.plone.org/svn/collective/raptus.securelogin',
+      url='https://github.com/Raptus/raptus.securelogin',
       license='GPL',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['raptus', 'raptus'],
